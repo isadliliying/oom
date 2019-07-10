50e89fa99e0f33131a2305b9afad16566b04c6f9
isadliliying <isadliliying@163.com>
2019-07-10 20:23:17 +0800

    测试搭建初始目录

---
+++ oom-userservice/src/main/java/com/aiseot/oom/userservice/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.aiseot.oom.userservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return "hello";
+    }
+}
